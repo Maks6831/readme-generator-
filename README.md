3fc5673a8a5e65822af675d36d47aab3475d7b42
Maks6831 <ma.khan6971@gmail.com>
2023-02-26 17:21:25 +0000

    Have a basic functional app now

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# readme-generator-
-generates Markdown files using NodeJS
+#  that your node_modules directory isn't tracked or uploaded to GitHub. Be sure to create your .gitignore file before installing any npm dependencies
+## Description
+e that your repo includes a package.json with the required dependencies. You can create one by running npm init when you first set up the project, before installing any dependencies.
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+## Installation
+A sample README generated using the application must be submitted.
+## Usage 
+Your GitHub repository containing your application code.
+## License
+MIT
+## Contributing 
+rfhuirfhui
+## Tests
+rhfurifhrufi
+## Questions 
+If you have any queries please feel free to contact me : rfhrui
+Github: frhui
+    
--- index.js
+++ index.js
@@ -1,20 +1,95 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+//const fs = require("fs");
+import fs from 'fs';
+//const path = require('path');
+import path from 'path';
+//const inquirer = require("inquirer");
+import inquirer from 'inquirer';
+//const generateMarkdown = require("./utils/generateMarkdown");
+//import generateMarkdown from '../utils/generateMarkdown';
 
 // array of questions for user
 const questions = [
-
+    'what is the title of your project?',
+    'Please provide a description',
+    'Please provide installation process',
+    'Please provide the usage',
+    'Please choose a license',
+    'Please provide any contributions',
+    'Describe any testing procedures',
+    'Please provide your github username',
+    'Please provide your email address'
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
+
+const writeToFile = (data) => {
+    fs.writeFile('README.md',
+    `# ${data[0]}
+    ## Description
+    ${data[1]}
+    ## Table of Contents
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributing](#contributing)
+    5. [Tests](#tests)
+    6. [Questions](#questions)
+    ## Installation
+    ${data[2]}
+    ## Usage 
+    ${data[3]}
+    ## License
+    ${data[4]}
+    ## Contributing 
+    ${data[5]}
+    ## Tests
+    ${data[6]}
+    ## Questions 
+
+    If you have any queries please feel free to contact me : ${data[8]}
+    Github: ${data[7]}
+    `
+    ,(err) => err? console.log(err): console.log('success'));
 }
+
+// creates question objects to parse to inquire prompt
+const execInquirer = (inquirerArr) => {
+    inquirer.prompt(inquirerArr).then((data) => {
+        console.log(data);
+        writeToFile(data);
+    });
+}
+
+const inquirerPrompts = (questions) => {
+    let inquirerArr = [];
+    for(let i =0; i < questions.length; i++){
+        if(questions[i].includes('license')){
+            let inquireQ = {
+                type:'list',
+                name: i.toString(),
+                message: questions[i],
+                choices: ['MIT', 'GPLv2', 'Apache', 'BSD 3-clause']
+            }
+            inquirerArr.push(inquireQ);
+        } else {
+            let inquireQ = {
+                name: i.toString(),
+                message: questions[i],
+            }
+            inquirerArr.push(inquireQ);
+        }
+        }     
+console.log(inquirerArr);
+execInquirer(inquirerArr);
+}
+
+
+// function to write README file
+//function writeToFile(fileName, data) {
+//}
 
 // function to initialize program
 function init() {
-
+    inquirerPrompts(questions)
 }
 
 // function call to initialize program
